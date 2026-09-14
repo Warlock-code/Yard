e1a9ee92409cf9c68451e0513122980230eb2d54
Anim Bronyina <animbronyinaa@gmail.com>
2026-09-14 22:22:13 +0000

    sfd

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,10 +4,21 @@ const config: CapacitorConfig = {
   appId: 'me.yardapp.app',
   appName: 'Yard',
   webDir: 'public',
- server: {
-  url: 'https://yardapp.me',
-  cleartext: false
-}
+  server: {
+    url: 'https://yardapp.me',
+    cleartext: false,
+    allowNavigation: [
+      'yardapp.me',
+      '*.yardapp.me'
+    ]
+  },
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 1200,
+      backgroundColor: "#050505",
+      showSpinner: false
+    }
+  }
 }
 
 export default config
