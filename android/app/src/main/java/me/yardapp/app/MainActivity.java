08d23520372baff3f86bd763af9ca0c1493634f6
Anim Bronyina <animbronyinaa@gmail.com>
2026-09-17 14:48:36 +0000

    Request Android notification permission natively

--- android/app/src/main/java/me/yardapp/app/MainActivity.java
+++ android/app/src/main/java/me/yardapp/app/MainActivity.java
@@ -1,5 +1,24 @@
 package me.yardapp.app;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+	private static final int NOTIFICATION_PERMISSION_REQUEST = 7001;
+
+	@Override
+	public void onStart() {
+		super.onStart();
+
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU
+			&& checkSelfPermission(Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+			requestPermissions(
+				new String[] { Manifest.permission.POST_NOTIFICATIONS },
+				NOTIFICATION_PERMISSION_REQUEST
+			);
+		}
+	}
+}
