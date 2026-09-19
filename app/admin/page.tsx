dfbb1da79b53013dc777fe10cd585044005a7f27
Anim Bronyina <animbronyinaa@gmail.com>
2026-09-19 17:43:10 +0000

    Cleanup: widen admin content area, remove temp IJ verify script

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -740,7 +740,7 @@ export default function AdminPage() {
         </div>
 
         {/* Content area */}
-        <main className="flex-1 px-4 md:px-8 py-6 max-w-5xl w-full mx-auto md:mx-0 relative z-10" ref={mainRef}>
+        <main className="flex-1 px-4 md:px-8 py-6 w-full relative z-10" ref={mainRef}>
           <AnimatePresence mode="wait">
             <motion.div
               key={section}
